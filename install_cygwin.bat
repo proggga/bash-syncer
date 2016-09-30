1dc289d3cdf73bac62e17714d35cd186a34264da
Mikhail <proggga@gmail.com>
2016-09-30 16:31:54 +0400

    fixed install_cygwin.bat

--- install_cygwin.bat
+++ install_cygwin.bat
@@ -4,11 +4,11 @@ echo "%PROCESSOR_ARCHITECTURE%" | find /i "x86" > NUL && set OS=86 || set OS=86_
 echo %OS%
 set URL=https://cygwin.com/setup-x%OS%.exe
 set CYGWIN=%TEMP%\cygwin_installer.exe
-set TERMINAL=C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico -
+set TERMINAL=C:\cygwin64\bin\mintty.exe
 if not exist %CYGWIN% bitsadmin /Transfer downloadcygwinjob %URL% %CYGWIN%
-if not exist %TERMINAL% --quiet-mode --packages rsync,openssh,git
+if not exist %TERMINAL% %CYGWIN% --quiet-mode --packages rsync,openssh,git
 set str=%~dp0
 call set str=%%str:\=/%%
 call set str=%%str::=%%
-echo /cygdrive/%str%
-:: %TERMINAL% "/cygwin/%O
+echo /cygdrive/%str%/send_sshkey.sh
+%TERMINAL% -h always -e "/bin/bash" -l -i /cygdrive/%str%sync.sh
