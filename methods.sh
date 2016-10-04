cd616cbc6333cd4f7ce515ed3600b4e404001f40
Mikhail Fesenko <proggga@gmail.com>
2016-10-04 16:21:34 +0300

    add ssh after sync script, moved code to methods

--- methods.sh
+++ methods.sh
@@ -1,20 +1,74 @@
 #!/usr/bin/env bash
 
-function raise_error {
+STOPFILE=/tmp/stop_it_pls
+
+
+raise_error() {
     echo $1;
     exit 2;
 }
 
 # main cycle which perform syncing
 # argument is file for syncing
-function syncing_cycle {
-    if [ "$1" ] ; then
-        echo "$1" | while read line ; do
-            # for ssh Tunnel
-            #time rsync -arpvzP --delete -e 'ssh -p12222 -o ControlPath="'$HOME'/.ssh/ctl/%L-%r@%h_%p"' $line $login@$server:~/tmp/;
-            time rsync -arpvzP --delete -e "ssh -p$port" "$line" $login@$server:~/tmp/;
-        done;
-    else
-         time rsync -arpvzP --delete -e 'ssh -p12222 -o ControlPath="'$HOME'/.ssh/ctl/%L-%r@%h_%p"' . $login@$server:~/tmp/;
+sync_to_remote() {
+    # for ssh Tunnel
+    #time rsync -arpvzP --delete -e 'ssh -p12222 -o ControlPath="'$HOME'/.ssh/ctl/%L-%r@%h_%p"' $line $login@$server:~/tmp/;
+     #echo rsync -arpvzP --delete -e "ssh -p$port" . $login@$server:"$remotedir";
+     rsync -arpvzP --delete -e "ssh -p$port" . $login@$server:"$remotedir" 2>&1 >/dev/null;
+}
+
+get_directory_from_terminal() {
+    localdir=$1
+    if [ -z "$1" ] ; then
+        read -e -p "Please input directory of project: " localdir;
+        localdir=$(echo $localdir | sed "s|~|$HOME|g")
     fi;
+    if [ -z "$localdir" ] ; then
+        raise_error "You should define directory to work";
+    fi;
+    if [ ! -d "$localdir" ] ; then raise_error "Directory does not exists"; fi;
+}
+
+stop_syncing() {
+    touch $STOPFILE;
+}
+# main cycle which can sync everything
+syncing_cycle() {
+    touch /tmp/timing_cycle;
+    last_changed="";
+    last_md5sum="";
+    while [ ! -f $STOPFILE ];
+    do
+        rm /tmp/newerthan;
+        touch -d '-0.2 seconds' /tmp/newerthan;
+        changed="";
+        filelist=$(find -name "*[^.]*" -type f -newer /tmp/newerthan 2>/dev/null);
+        if [ "$filelist" ] && [ "$filelist" != "$last_changed" ]; then
+            #echo "changed";
+            #echo "$filelist";
+            last_changed="$filelist";
+            changed="true";
+            last_md5sum=$(find . | md5sum);
+        else
+            last_changed="";
+        fi;
+        md5=$(find . | md5sum);
+        if [ "$md5" != "$last_md5sum" ] ; then
+            #echo "deleted";
+            changed="true";
+            last_md5sum=$md5
+        fi;
+        #echo "waiting"
+        if [ "$changed" ] ; then
+            changed="";
+            sync_to_remote;
+            touch /tmp/timing_cycle;
+        else
+            touch -d '-2 seconds' /tmp/newerthan;
+            more_than_2seconds=$(find /tmp/timing_cycle ! -newer /tmp/newerthan)
+            if [ "$more_than_2seconds" ] ; then sync_to_remote; touch /tmp/timing_cycle; fi;
+        fi;
+        sleep 0.1;
+    done;
+    rm $STOPFILE;
 }
--- sync.sh
+++ sync.sh
@@ -2,60 +2,17 @@
 
 # IMPORT main functions from methods.sh
 . methods.sh
-# list of methods:
-# raise_error (string) :: method print message to screen and exit with state 2
 
 # settings
 . settings.conf
 
+
 # working directory
-localdir=$1
-if [ -z "$1" ] ; then
-    read -e -p "Please input directory of project: " localdir;
-    localdir=$(echo $localdir | sed "s|~|$HOME|g")
-fi;
-if [ -z "$localdir" ] ; then
-    raise_error "You should define directory to work";
-fi;
-if [ ! -d "$localdir" ] ; then raise_error "Directory does not exists"; fi;
+currentdir=$(pwd)
 
-# connection
-last_changed="";
-last_md5sum="";
+get_directory_from_terminal $1
+cd $localdir
 
-# check catalog existance (for ssh tunnel)
-#if [ ! -d "$HOME/.ssh/ctl" ] ; then
-#    mkdir -p "$HOME/.ssh/ctl";
-#fi;
-
-# Ssh tullel works only at linux)
-#ssh -nNf -p12222 -o ControlMaster=yes -o ControlPath="\"$HOME/.ssh/ctl/%L-%r@%h_%p\"" $login@$server
-
-
-while :;
-do
-    rm /tmp/newerthan;
-    touch -d '-0.2 seconds' /tmp/newerthan;
-    changed="";
-    filelist=$(find * -type f ! -path . -newer /tmp/newerthan 2>/dev/null);
-    if [ "$filelist" ] && [ "$filelist" != "$last_changed" ]; then
-        echo "changed";
-        echo "$filelist";
-        last_changed="$filelist";
-        changed="true";
-        syncing_cycle "$filelist";
-        last_md5sum=$(find . | md5sum);
-    else
-        last_changed="";
-    fi;
-    md5=$(find . | md5sum);
-    if [ "$md5" != "$last_md5sum" ] ; then
-        echo "deleted";
-        syncing_cycle;
-        last_md5sum=$md5
-    fi;
-    sleep 0.1;
-done;
-
-# Ssh tunnel (Works only at linux)
-#ssh -p12222 -O stop -S "$HOME/.ssh/ctl/%L-%r@%h_%p" $login@$server
+syncing_cycle &
+cd $currentdir
+ssh -p$port $login@$server
